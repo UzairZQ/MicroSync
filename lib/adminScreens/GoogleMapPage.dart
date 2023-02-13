9f5eb17d330e75a42c5278abfca10145cc1d580d
UzairZQ <uzairqureshi9@gmail.com>
2023-02-13 11:40:27 +0500

    added location tracking feature but i might have some more refinements to do

--- lib/adminScreens/GoogleMapPage.dart
+++ lib/adminScreens/GoogleMapPage.dart
@@ -2,47 +2,62 @@
 
 import 'dart:async';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:micro_pharma/components/constants.dart';
+import 'package:micro_pharma/services/database.dart';
+import 'package:micro_pharma/services/location_services.dart';
+import 'package:provider/provider.dart';
+
+import 'package:location/location.dart' as loc;
 
 class GoogleMapPage extends StatefulWidget {
   static const String id = 'map_page';
-  GoogleMapPage({Key? key}) : super(key: key);
+
+  final String userId;
+
+  GoogleMapPage({required this.userId});
 
   @override
   State<GoogleMapPage> createState() => _GoogleMapPageState();
 }
 
 class _GoogleMapPageState extends State<GoogleMapPage> {
-  static const CameraPosition _atdPosition =
-      CameraPosition(target: LatLng(34.1688, 73.2215), zoom: 14.4746);
-  final Completer<GoogleMapController> _controller = Completer();
-  final List<Marker> _markers = <Marker>[
-    const Marker(
-      markerId: MarkerId('1'),
-      position: LatLng(34.1688, 73.2215),
-      infoWindow: InfoWindow(title: 'Initial Location'),
-    ),
-  ];
+  final loc.Location location = loc.Location();
+  late GoogleMapController _controller;
+  bool _added = false;
 
-  Future<Position> getUserCurrentLocation() async {
-    await Geolocator.requestPermission()
-        .then((value) {})
-        .onError((error, stackTrace) {
-      print('error' + error.toString());
-    });
+  // static const CameraPosition _atdPosition =
+  //     CameraPosition(target: LatLng(34.1688, 73.2215), zoom: 14.4746);
+  // final Completer<GoogleMapController> _controller = Completer();
+  // final List<Marker> _markers = <Marker>[
+  //   const Marker(
+  //     markerId: MarkerId('1'),
+  //     position: LatLng(34.1688, 73.2215),
+  //     infoWindow: InfoWindow(title: 'Initial Location'),
+  //   ),
+  // ];
 
-    return await Geolocator.getCurrentPosition();
-  }
+  // Future<Position> getUserCurrentLocation() async {
+  //   await Geolocator.requestPermission()
+  //       .then((value) {})
+  //       .onError((error, stackTrace) {
+  //     print('error' + error.toString());
+  //   });
+
+  //   return await Geolocator.getCurrentPosition();
+  // }
 
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
     print('disposed google maps');
+    // Provider.of<LocationServices>(context).stopListening();
+    LocationServices().stopListening();
   }
 
   @override
@@ -56,40 +71,53 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
       // ),
       // leading:
 
-      body: GoogleMap(
-        initialCameraPosition: _atdPosition,
-        markers: Set<Marker>.of(_markers),
-        zoomControlsEnabled: false,
-        mapType: MapType.normal,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
+      body: StreamBuilder(
+        stream: DataBaseService().streamUser(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (_added) {
+            mymap(snapshot);
+          }
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return GoogleMap(
+            mapType: MapType.normal,
+            markers: {
+              Marker(
+                markerId: MarkerId('Current User Location'),
+                icon: BitmapDescriptor.defaultMarkerWithHue(
+                    BitmapDescriptor.hueRed),
+                position: LatLng(
+                  snapshot.data!.docs.singleWhere(
+                      (element) => element.id == widget.userId)['latitude'],
+                  snapshot.data!.docs.singleWhere(
+                      (element) => element.id == widget.userId)['longitude'],
+                ),
+              ),
+            },
+            initialCameraPosition: CameraPosition(
+              target: LatLng(
+                snapshot.data!.docs.singleWhere(
+                    (element) => element.id == widget.userId)['latitude'],
+                snapshot.data!.docs.singleWhere(
+                    (element) => element.id == widget.userId)['longitude'],
+              ),
+              zoom: 14.47,
+            ),
+            onMapCreated: (GoogleMapController controller) async {
+              setState(() {
+                _controller = controller;
+                _added = true;
+              });
+            },
+          );
         },
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
-          getUserCurrentLocation().then(
-            (value) async {
-              print('my current locaton');
-              print(
-                  value.latitude.toString() + " " + value.longitude.toString());
-
-              _markers.add(
-                Marker(
-                  markerId: const MarkerId('2'),
-                  position: LatLng(value.latitude, value.longitude),
-                  infoWindow: const InfoWindow(title: 'Current User Location'),
-                ),
-              );
-
-              CameraPosition cameraPosition = CameraPosition(
-                  target: LatLng(value.latitude, value.longitude), zoom: 18);
-
-              final GoogleMapController controller = await _controller.future;
-              controller.animateCamera(
-                  CameraUpdate.newCameraPosition(cameraPosition));
-              setState(() {});
-            },
-          );
+          LocationServices().getLocation(widget.userId);
         },
         label: const Text(
           'Get User Location',
@@ -101,4 +129,55 @@ class _GoogleMapPageState extends State<GoogleMapPage> {
       ),
     );
   }
+
+  Future<void> mymap(AsyncSnapshot<QuerySnapshot> snapshot) async {
+    await _controller
+        .animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+            target: LatLng(
+              snapshot.data!.docs.singleWhere(
+                  (element) => element.id == widget.userId)['latitude'],
+              snapshot.data!.docs.singleWhere(
+                  (element) => element.id == widget.userId)['longitude'],
+            ),
+            zoom: 14.47)));
+  }
 }
+
+
+
+
+// GoogleMap(
+//         initialCameraPosition: _atdPosition,
+//         markers: Set<Marker>.of(_markers),
+//         zoomControlsEnabled: false,
+//         mapType: MapType.normal,
+//         onMapCreated: (GoogleMapController controller) {
+//           _controller.complete(controller);
+//         },
+//       ),
+
+
+
+//  getUserCurrentLocation().then(
+//             (value) async {
+//               print('my current locaton');
+//               print(
+//                   value.latitude.toString() + " " + value.longitude.toString());
+
+//               _markers.add(
+//                 Marker(
+//                   markerId: const MarkerId('2'),
+//                   position: LatLng(value.latitude, value.longitude),
+//                   infoWindow: const InfoWindow(title: 'Current User Location'),
+//                 ),
+//               );
+
+//               CameraPosition cameraPosition = CameraPosition(
+//                   target: LatLng(value.latitude, value.longitude), zoom: 18);
+
+//               final GoogleMapController controller = await _controller.future;
+//               controller.animateCamera(
+//                   CameraUpdate.newCameraPosition(cameraPosition));
+//               setState(() {});
+//             },
+//           );
--- lib/userScreens/homepage.dart
+++ lib/userScreens/homepage.dart
@@ -2,6 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:micro_pharma/components/containerRow.dart';
 import 'package:micro_pharma/components/constants.dart';
+import 'package:micro_pharma/services/database.dart';
+import 'package:micro_pharma/services/location_services.dart';
 import 'package:micro_pharma/userScreens/call_planner.dart';
 import 'package:micro_pharma/userScreens/daily_call_report.dart';
 import 'package:micro_pharma/userScreens/user_dashboard.dart';
@@ -10,184 +12,219 @@ import 'package:micro_pharma/userScreens/login_page.dart';
 import 'package:micro_pharma/userScreens/master_screen.dart';
 import 'package:micro_pharma/userScreens/product_order.dart';
 import 'package:micro_pharma/userScreens/userSettings.dart';
+import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+import 'package:location/location.dart';
 import '../main.dart';
 
-class HomePage extends StatelessWidget {
+final currentUser = FirebaseAuth.instance.currentUser;
+
+class HomePage extends StatefulWidget {
   static String id = 'home';
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // context.read<LocationServices>().requestPermission();
+    // context.watch<LocationServices>().listenLocation(currentUser!.uid);
+    userLocation();
+  }
+
+  void userLocation() async {
+    LocationServices().requestPermission();
+    LocationServices().getLocation(currentUser!.uid);
+    await LocationServices().listenLocation(currentUser!.uid);
+    Location()
+        .changeSettings(interval: 1000, accuracy: LocationAccuracy.balanced);
+    Location().enableBackgroundMode(enable: true);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final currentUser = FirebaseAuth.instance.currentUser;
     print(currentUser!.email);
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.logout_outlined),
-        onPressed: () async {
-          FirebaseAuth.instance.signOut();
-          Navigator.pushNamed(context, LoginPage.id);
-          var sharedLogin = await SharedPreferences.getInstance();
-          sharedLogin.setBool(SplashPageState.KEYLOGIN, false);
-          var sharedUser = await SharedPreferences.getInstance();
-          sharedUser.setBool(SplashPageState.KEYUSER, false);
-        },
-      ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 250.0,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  color: Color(0xFF1FB7CC),
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(40.0),
-                    bottomRight: Radius.circular(40.0),
+    return Provider<String>(
+      create: (context) => currentUser!.uid.toString(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.logout_outlined),
+          onPressed: () async {
+            FirebaseAuth.instance.signOut();
+            setState(() {
+              LocationServices().stopListening();
+            });
+
+            Navigator.pushNamed(context, LoginPage.id);
+            var sharedLogin = await SharedPreferences.getInstance();
+            sharedLogin.setBool(SplashPageState.KEYLOGIN, false);
+            var sharedUser = await SharedPreferences.getInstance();
+            sharedUser.setBool(SplashPageState.KEYUSER, false);
+          },
+        ),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  height: 250.0,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    color: Color(0xFF1FB7CC),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(40.0),
+                      bottomRight: Radius.circular(40.0),
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                        'Welcome User!',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0,
+                          //color: Colors.white,
+                          foreground: Paint()..color = Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 17.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(
+                            Icons.calendar_month,
+                            color: Colors.white,
+                          ),
+                          Flexible(
+                            child: Text(
+                              'Today\'s Scheduled Plan: Abbottabad to Mansehra',
+                              style: TextStyle(
+                                fontFamily: 'Poppins',
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 15.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(
+                            Icons.medication_rounded,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            'Doctors: 8',
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 17.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 20.0,
+                          ),
+                          Icon(
+                            Icons.medical_services_rounded,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            'Chemists:5',
+                            style: TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 17.0,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(height: 25.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          kbuttonstyle(
+                              onPressed: () {},
+                              color: const Color(0xFF009AAF),
+                              text: 'Start Your Day'),
+                          const SizedBox(
+                            width: 15.0,
+                          ),
+                          kbuttonstyle(
+                              onPressed: () {},
+                              color: const Color.fromARGB(255, 171, 75, 95),
+                              text: 'Change Plan'),
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Text(
-                      'Welcome User!',
-                      style: TextStyle(
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0,
-                        //color: Colors.white,
-                        foreground: Paint()..color = Colors.white,
-                      ),
-                    ),
-                    const SizedBox(height: 17.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Icon(
-                          Icons.calendar_month,
-                          color: Colors.white,
-                        ),
-                        Text(
-                          'Today\'s Scheduled Plan: Abbottabad to Mansehra',
-                          style: TextStyle(
-                            fontFamily: 'Poppins',
-                            fontSize: 15.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 15.0,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: const [
-                        Icon(
-                          Icons.medication_rounded,
-                          color: Colors.white,
-                        ),
-                        Text(
-                          'Doctors: 8',
-                          style: TextStyle(
-                            fontFamily: 'Poppins',
-                            fontSize: 17.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                        SizedBox(
-                          width: 20.0,
-                        ),
-                        Icon(
-                          Icons.medical_services_rounded,
-                          color: Colors.white,
-                        ),
-                        Text(
-                          'Chemists:5',
-                          style: TextStyle(
-                            fontFamily: 'Poppins',
-                            fontSize: 17.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        )
-                      ],
-                    ),
-                    const SizedBox(height: 25.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        kbuttonstyle(
-                            onPressed: () {},
-                            color: const Color(0xFF009AAF),
-                            text: 'Start Your Day'),
-                        const SizedBox(
-                          width: 15.0,
-                        ),
-                        kbuttonstyle(
-                            onPressed: () {},
-                            color: const Color.fromARGB(255, 171, 75, 95),
-                            text: 'Change Plan'),
-                      ],
-                    ),
-                  ],
+                const SizedBox(height: 20.0),
+                containerRow(
+                  container1Clr: const Color(0xFFF0DCFF),
+                  container1Icon: Icons.dashboard_outlined,
+                  container1Text: 'Dashboard',
+                  container1Tap: () =>
+                      Navigator.pushNamed(context, Dashboard.id),
+                  container2Clr: const Color(0xFFFFC8C8),
+                  container2Icon: Icons.calendar_month_outlined,
+                  container2Text: 'Call Planner',
+                  container2Tap: () =>
+                      Navigator.pushNamed(context, CallPlanner.id),
                 ),
-              ),
-              const SizedBox(height: 20.0),
-              containerRow(
-                container1Clr: const Color(0xFFF0DCFF),
-                container1Icon: Icons.dashboard_outlined,
-                container1Text: 'Dashboard',
-                container1Tap: () => Navigator.pushNamed(context, Dashboard.id),
-                container2Clr: const Color(0xFFFFC8C8),
-                container2Icon: Icons.calendar_month_outlined,
-                container2Text: 'Call Planner',
-                container2Tap: () =>
-                    Navigator.pushNamed(context, CallPlanner.id),
-              ),
-              const SizedBox(
-                height: 30.0,
-              ),
-              containerRow(
-                container1Clr: const Color.fromARGB(255, 133, 254, 226),
-                container1Icon: Icons.assignment_outlined,
-                container1Text: 'Day Plan',
-                container1Tap: () => Navigator.pushNamed(context, DayPlan.id),
-                container2Clr: const Color(0xffFFE974),
-                container2Icon: Icons.assignment_turned_in_outlined,
-                container2Text: 'Daily Call Report',
-                container2Tap: () =>
-                    Navigator.pushNamed(context, DailyCallReport.id),
-              ),
-              const SizedBox(
-                height: 30.0,
-              ),
-              containerRow(
-                container1Clr: Colors.blue.shade200,
-                container1Icon: Icons.medical_services_outlined,
-                container1Text: 'Doctors, Areas & Chemists',
-                container2Clr: Colors.orange.shade200,
-                container2Icon: Icons.add_shopping_cart_outlined,
-                container2Text: 'Orders',
-                container1Tap: () => Navigator.pushNamed(context, Master.id),
-                container2Tap: () =>
-                    Navigator.pushNamed(context, ProductOrder.id),
-              ),
-              const SizedBox(
-                height: 15.0,
-              ),
-              kbuttonstyle(
-                  color: kappbarColor,
-                  text: 'Settings',
-                  onPressed: () {
-                    Navigator.pushNamed(context, UserSettings.id);
-                  }),
-            ],
+                const SizedBox(
+                  height: 30.0,
+                ),
+                containerRow(
+                  container1Clr: const Color.fromARGB(255, 133, 254, 226),
+                  container1Icon: Icons.assignment_outlined,
+                  container1Text: 'Day Plan',
+                  container1Tap: () => Navigator.pushNamed(context, DayPlan.id),
+                  container2Clr: const Color(0xffFFE974),
+                  container2Icon: Icons.assignment_turned_in_outlined,
+                  container2Text: 'Daily Call Report',
+                  container2Tap: () =>
+                      Navigator.pushNamed(context, DailyCallReport.id),
+                ),
+                const SizedBox(
+                  height: 30.0,
+                ),
+                containerRow(
+                  container1Clr: Colors.blue.shade200,
+                  container1Icon: Icons.medical_services_outlined,
+                  container1Text: 'Doctors, Areas & Chemists',
+                  container2Clr: Colors.orange.shade200,
+                  container2Icon: Icons.add_shopping_cart_outlined,
+                  container2Text: 'Orders',
+                  container1Tap: () => Navigator.pushNamed(context, Master.id),
+                  container2Tap: () =>
+                      Navigator.pushNamed(context, ProductOrder.id),
+                ),
+                const SizedBox(
+                  height: 15.0,
+                ),
+                kbuttonstyle(
+                    color: kappbarColor,
+                    text: 'Settings',
+                    onPressed: () {
+                      Navigator.pushNamed(context, UserSettings.id);
+                    }),
+              ],
+            ),
           ),
         ),
       ),
